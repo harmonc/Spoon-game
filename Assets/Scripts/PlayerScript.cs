e0427f269a0b0c8ac6855cc0c73f7c05a8569f50
Caleb Harmon <calebmharmon@gmail.com>
2020-10-01 12:04:47 -0500

    added clamp

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,7 +8,7 @@ public class PlayerScript : MonoBehaviour
     private float movementX;
     private float movementY;
     private Rigidbody2D rb;
-    float speed = 10.0f;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,9 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, movementY, 0.0f);
-        rb.AddForce(movement * speed);
+        rb.velocity = movement*speed*Time.deltaTime;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.0f, 4.0f),Mathf.Clamp(transform.position.y, -2.0f, 2.0f),transform.position.z);
+
     }
 
     void OnMove(InputValue movementValue)
