a856555895dfa34b4089af2f541e60af2f0666c3
Caleb Harmon <calebmharmon@gmail.com>
2020-10-01 15:51:29 -0500

    tweaked clamp range to match screen

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,10 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public Vector2 xBounds;
+    public Vector2 yBounds;
+
     private float movementX;
     private float movementY;
     private Rigidbody2D rb;
-    public float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +24,11 @@ public class PlayerScript : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, movementY, 0.0f);
         rb.velocity = movement*speed*Time.deltaTime;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.0f, 4.0f),Mathf.Clamp(transform.position.y, -2.0f, 2.0f),transform.position.z);
+    }
 
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xBounds.x, xBounds.y), Mathf.Clamp(transform.position.y, yBounds.x, yBounds.y), transform.position.z);
     }
 
     void OnMove(InputValue movementValue)
