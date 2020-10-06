82b2c875e763329851a2d61b1b0a86fc1ca5a9e5
Caleb Harmon <calebmharmon@gmail.com>
2020-10-06 11:42:06 -0500

    icecream have random colors

--- Assets/Scripts/IcecreamScript.cs
+++ Assets/Scripts/IcecreamScript.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class IcecreamScript : MonoBehaviour
 {
+    public Sprite chocolate;
+    public Sprite strawberry;
+    public Sprite vanilla;
+    public Sprite coffee;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer cream = this.GetComponent<SpriteRenderer>();
+        int r = Mathf.FloorToInt(Random.Range(0, 4));
+        if (r == 0)
+        {
+            cream.sprite = chocolate;
+        }
+        else if (r == 1)
+        {
+            cream.sprite = strawberry;
+        }
+        else if (r == 2)
+        {
+            cream.sprite = vanilla;
+        }
+        else if (r == 3)
+        {
+            cream.sprite = coffee;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < -45) {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,7 +50,6 @@ public class IcecreamScript : MonoBehaviour
                 top.transform.position = this.gameObject.transform.position + new Vector3(0.0f,1.0f,-1.0f);
                 Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D>();
                 Destroy(rb);
-                Debug.Log("test2");
             }
         }
     }   
