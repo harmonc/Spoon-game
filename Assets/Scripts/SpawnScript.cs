0138b7a1ca4634411ae62e6168ac37173faea7d3
Caleb Harmon <calebmharmon@gmail.com>
2020-10-05 20:54:12 -0500

    added a spawner and spanwer script for icecream

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject spawnObject;
+    public int y;
+    public Vector2 range;
+    public int frequency;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > frequency) {
+            time = 0;
+            GameObject newObject = Instantiate(spawnObject);
+            newObject.transform.position = new Vector3(Random.Range(range.x, range.y), y, 0.0f);
+        }
+    }
+}
